79b6967b53bea5a163013bbe685eb248c916389a
smruti164 <110184881+smruti164@users.noreply.github.com>
2026-05-01 02:36:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../firebase/firebase";
-import Home from "../pages/Home";
-import Admin from "../pages/Admin";
+import { auth } from "./firebase/firebase";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
 export default function App() {
   const [user, setUser] = useState(null);
