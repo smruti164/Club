fca4b1481b28e2409070316ba8ffb02314da96ac
smruti164 <110184881+smruti164@users.noreply.github.com>
2026-05-01 02:12:53 +0530

    Update Gallery.jsx

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,1 +1,16 @@
+export default function Gallery() {
+  return (
+    <section style={{ padding: 50 }}>
+      <h2>Gallery</h2>
 
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit,minmax(200px,1fr))",
+        gap: 10
+      }}>
+        <img src="/gallery/1.jpg" />
+        <img src="/gallery/2.jpg" />
+      </div>
+    </section>
+  );
+}
