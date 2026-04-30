39a7d8267a3a73baccbcef98d41704a738e1db45
smruti164 <110184881+smruti164@users.noreply.github.com>
2026-05-01 02:13:17 +0530

    Update Login.jsx

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,1 +1,23 @@
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+
+  return (
+    <div style={{ padding: 30 }}>
+      <h2>Admin Login</h2>
+
+      <input placeholder="email" onChange={e => setEmail(e.target.value)} />
+      <input type="password" placeholder="password" onChange={e => setPass(e.target.value)} />
+
+      <button onClick={() =>
+        signInWithEmailAndPassword(auth, email, pass)
+      }>
+        Login
+      </button>
+    </div>
+  );
+}
