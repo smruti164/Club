850d4e6f3d6c0bebfe5118c4f8976bb4e39ae0e2
smruti164 <110184881+smruti164@users.noreply.github.com>
2026-05-01 02:21:57 +0530

    Update Admin.jsx

--- pages/Admin.jsx
+++ pages/Admin.jsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
-import Login from "../components/Login";
+import AdminMembers from "../components/AdminMembers";
+import AdminEvents from "../components/AdminEvents";
 
 export default function Admin() {
-  const [tab, setTab] = useState("events");
+  const [tab, setTab] = useState("members");
 
   return (
     <div style={{ padding: 30 }}>
-      <h2>Admin Dashboard</h2>
+      <h1>CMS Admin Panel</h1>
 
       <div style={{ display: "flex", gap: 10 }}>
-        <button onClick={() => setTab("events")}>Events</button>
         <button onClick={() => setTab("members")}>Members</button>
-        <button onClick={() => setTab("gallery")}>Gallery</button>
+        <button onClick={() => setTab("events")}>Events</button>
       </div>
 
-      {tab === "events" && <div>Event CRUD UI here</div>}
-      {tab === "members" && <div>Member CRUD UI here</div>}
-      {tab === "gallery" && <div>Gallery Upload UI here</div>}
+      {tab === "members" && <AdminMembers />}
+      {tab === "events" && <AdminEvents />}
     </div>
   );
 }
